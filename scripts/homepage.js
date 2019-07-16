a5308ac10e3533315695896fbca29c917083b035
Tom <25043847+Douile@users.noreply.github.com>
2019-07-16 22:22:32 +0100

    Version 0.1

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -2,9 +2,7 @@
 function query(q) {
   return document.querySelector(q);
 }
-function getCookie(name) {
 
-}
 function generatePositions(poscount) {
   let width = window.innerWidth;
   let height = window.innerHeight;
@@ -15,27 +13,17 @@ function generatePositions(poscount) {
   }
 
   var size = (smallest/2)-200;
-  var offset = -1*size/2;
-  var centerSize = size.toString() + "px;";
-  var centerOffset = offset.toString() + "px;";
-  let style = "".concat("height:",centerSize,"width:",centerSize,"margin-left:",centerOffset,"margin-top:",centerOffset);
-  let centr = query(".maincont");
-  centr.setAttribute("style",style);
 
   var output = "";
 
   var positions = Array(poscount);
-  let mult = 360/poscount;
+  let mult = (Math.PI*2)/poscount;
   let r = size/2 + 150;
   for (a=0;a<positions.length;a++) {
-    var deg = mult*a;
-    let x = r * Math.cos(deg * Math.PI / 180);
-    let y = r * Math.sin(deg * Math.PI / 180);
-    if (deg > 180) {
-      deg = deg-360;
-    }
-    var degrees = (deg).toString() + "deg";
-    let style = "".concat('.contLink[pos="',a,'"] { transform: translate(',x,'px,',y,'px); }' /*.content[pos="',a,'"] > .contLine {transform: rotate(',degrees,') translate(',xin,'px,',yin,'px);} '*/);
+    var rad = mult*a - Math.PI/2;
+    let x = r * Math.cos(rad);
+    let y = r * Math.sin(rad);
+    let style = "".concat('.contLink[pos="',a,'"] { transform: translate(',x,'px,',y,'px); }' );
     positions[a] = style;
     output += style;
   }
@@ -45,6 +33,7 @@ function generatePositions(poscount) {
   document.head.appendChild(st);
   return positions;
 }
+
 function setupBoxes(poscount) {
   var cont = query(".maincont");
   for (a=0;a<poscount;a++) {
@@ -56,8 +45,9 @@ function setupBoxes(poscount) {
     cont.appendChild(li);
   }
 }
+
 function mouseHandler(e) {
-  if (e.target.getAttribute("class") == "contLink" && e.type == "contextmenu") {
+  if (e.target.getAttribute("class") === "contLink" && e.type === "contextmenu") {
     e.preventDefault();
     let id = e.target.getAttribute("pos");
     query("#linkform").setAttribute("linkid",id);
@@ -66,22 +56,26 @@ function mouseHandler(e) {
     query("#linkurl").value = href;
     query("#linkfavicon").value = favicon;
     query("#linkfaviconprev").src = favicon;
-  } else if (e.target.id == "linksubmit" && e.type == "click") {
+  } else if (e.target.id === "linksubmit" && e.type === "click") {
     let urlel = query("#linkurl");
     let faviconel = query("#linkfavicon");
     var url = urlel.value;
-    if (url != "") {
-      var favicon = faviconel.value;
-      urlel.value = "";
-      faviconel.value = "";
-      var id = query("#linkform").getAttribute("linkid");
-      var el = query("".concat('.contLink[pos="',id,'"]'));
-      el.href = url;
-      el.firstChild.src = favicon;
-    }
+    let id = query("#linkform").getAttribute("linkid");
+    var favicon = faviconel.value;
+    urlel.value = "";
+    faviconel.value = "";
+    window.links[id] = { url: url, ic: favicon };
+    saveLinks(window.links);
+    refreshLinks();
     query("#linkform").removeAttribute("linkid");
+  } else if (e.target.classList.contains('count-button') && e.type === "click") {
+    if (e.target.classList.contains('count-increase')) window.links.push({ url: '', ic: '' });
+    if (e.target.classList.contains('count-decrease')) window.links.pop( window.links.length - 1);
+    saveLinks(window.links);
+    refreshLinks();
   }
 }
+
 function changeUrl(e) {
   var url = e.target.value;
   console.log(url);
@@ -101,15 +95,98 @@ function changeUrl(e) {
   }
 }
 
+function loadErrorHandler(e) {
+  if (e.target.tagName === 'IMG') {
+    e.target.removeAttribute('src');
+  }
+}
+
+function parseCookie() {
+  let data = {},
+  i = 0, cookieString = document.cookie,
+  name = '', value = '',
+  state = 0;
+  while (i < cookieString.length) {
+    let c = cookieString[i];
+    if (c === '=') {
+      state = 1;
+      value = '';
+    } else if (c === '&') {
+      console.log(name,value);
+      data[name] = value;
+      state = 0;
+      name = '';
+    } else {
+      if (state === 0) name += c;
+      else if (state === 1) value += c;
+    }
+    i++;
+  }
+  if (state === 1) data[name] = value;
+  return data;
+}
+
+function parseLinks(links) {
+  generatePositions(links.length);
+  setupBoxes(links.length);
+
+  for (let i=0;i<links.length;i++) {
+    let t = query(`.contLink[pos="${i}"]`);
+    t.href = links[i].url;
+    t.querySelector('img').src = links[i].ic;
+  }
+}
+
+function saveLinks(links) {
+  let data = JSON.stringify(links);
+  saveLinksCookie(data);
+  saveLinksLocalStorage(data);
+}
+
+function saveLinksCookie(data) {
+  document.cookie = `sphere-links=${encodeURIComponent(data)};path=/;samesite;expires=Fri, 1 Jan 9999 00:00:00 GMT`;
+}
+
+function saveLinksLocalStorage(data) {
+  window.localStorage.setItem('sphere-links',data);
+}
+
+function loadLinks() {
+  let data = loadLinksLocalStorage();
+  data = data === null ? loadLinksCookie() : data;
+  return data;
+}
+
+function loadLinksCookie() {
+  let text = decodeURIComponent( parseCookie()['sphere-links'] ),
+  data = {};
+  try {
+    data = JSON.parse(text);
+  } catch(e) {}
+  return data;
+}
+
+function loadLinksLocalStorage() {
+  let data = {};
+  try {
+    data = JSON.parse(window.localStorage.getItem('sphere-links'));
+  } catch(e) {}
+  return data;
+}
+
+function refreshLinks() {
+  window.links = loadLinks();
+  document.querySelectorAll('.contLink').forEach(n => n.remove());
+  parseLinks(window.links);
+}
+
 window.addEventListener("click",mouseHandler);
 window.addEventListener("contextmenu",mouseHandler);
+window.addEventListener("error",loadErrorHandler, { capture: true, passive: true }, true);
 query("#linkurl").addEventListener("keyup",changeUrl);
 
-var poscount = 8;
-var homepagedata = new Array(poscount);
-generatePositions(poscount);
-setupBoxes(poscount);
-var defaults = [{url:"https://duckduckgo.com/?kae=d&kax=-1&kad=en_GB&kac=-1&k1=-1&kaj=m&kam=osm&kak=-1&kaq=-1&kao=-1&kap=-1&kp=-2&kz=1&kt=a&ks=n&kw=s&km=l&ka=a&ku=-1&kai=1&kg=p",ic:"https://duckduckgo.com/assets/icons/meta/DDG-icon_256x256.png"},
+var links;
+/* [{url:"https://duckduckgo.com/?kae=d&kax=-1&kad=en_GB&kac=-1&k1=-1&kaj=m&kam=osm&kak=-1&kaq=-1&kao=-1&kap=-1&kp=-2&kz=1&kt=a&ks=n&kw=s&km=l&ka=a&ku=-1&kai=1&kg=p",ic:"https://duckduckgo.com/assets/icons/meta/DDG-icon_256x256.png"},
   {url:"https://www.youtube.com/",ic:"https://www.youtube.com/yts/img/favicon_144-vfliLAfaB.png"},
   {url:"https://google.co.uk/",ic:"https://avatars0.githubusercontent.com/u/1342004?s=400&v=4"},
   {url:"http://steamcommunity.com/",ic:"http://steamcommunity-a.akamaihd.net/public/shared/images/responsive/share_steam_logo.png"},
@@ -117,9 +194,6 @@ var defaults = [{url:"https://duckduckgo.com/?kae=d&kax=-1&kad=en_GB&kac=-1&k1=-
   {url:"https://twitter.com/",ic:"https://abs.twimg.com/icons/apple-touch-icon-192x192.png"},
   {url:"https://www.netflix.com/",ic:"https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png"},
   {url:"https://github.com/",ic:"https://assets-cdn.github.com/pinned-octocat.svg"}]
-  ,el;
-for (var i=0;i<defaults.length;i++) {
-  el = query("".concat(".contLink[pos=\"",i,"\"]"));
-  el.href = defaults[i].url;
-  el.firstChild.src = defaults[i].ic;
-}
+  */
+
+refreshLinks();
